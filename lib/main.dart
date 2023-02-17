16b69752bbf20533d02293530a17f8e4df4544d1
ssdd33 <beeblu.0@gmail.com>
2023-02-17 17:07:13 +0900

    [feat] pomodoro

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,26 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(SplashScreen());
 }
 
-class SplashScreen extends StatefulWidget {
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            color: Color(0xFFF99231),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/snoopy.png',
-                    width: 200,
-                  ),
-                  SizedBox(height: 12.0),
-                  CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation(Colors.white),
-                  ),
-                ],
-              ),
-            ],
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  bool isRunning = false;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
+  }
+
+  void onStartPressed() {
+    if (!isRunning) {
+      timer = Timer.periodic(
+        const Duration(seconds: 1),
+        onTick,
+      );
+      setState(() {
+        isRunning = true;
+      });
+    } else {
+      isRunning = false;
+      setState(() {
+        timer.cancel();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '$totalSeconds',
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+                child: IconButton(
+              iconSize: 120,
+              color: Theme.of(context).cardColor,
+              icon: Icon(isRunning
+                  ? Icons.stop_circle_outlined
+                  : Icons.play_circle_outline),
+              onPressed: onStartPressed,
+            )),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius:
+                          const BorderRadius.vertical(top: Radius.circular(50)),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodors',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge!
+                                  .color,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                              fontSize: 58,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge!
+                                  .color,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
